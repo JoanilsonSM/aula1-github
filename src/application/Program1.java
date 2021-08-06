99949cb43411d9e396a6c9c926a8111c01cbfd39
JoanilsonSM <joanilsonsm@gmail.com>
2021-08-06 16:03:51 -0300

    Projeto criado

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -5,14 +5,12 @@ import java.util.Date;
 import entities.Order;
 import entities.enums.OrderStatus;
 
-public class Program {
+public class Program1 {
 
 	public static void main(String[] args) {
 
 		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
 		
-		System.out.println(Order);
-
+		System.out.println(order);
 	}
-
 }
